a467e986b427eef2781cfc3cd143239ae48b2469
BruceRW3 <brwiggleston@gmail.com>
2022-03-24 12:55:58 -0400

    added working timer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function App() {
   const [text, setText] = useState('')
+  const [timer, setTimer] = useState(5)
+
+  useEffect(() => {
+    if(timer > 0) {
+      setTimeout(() => {
+        setTimer(time => time -1)
+      }, 1000)
+    }
+  }, [timer])
 
   function handleChange(e) {
     const {value} = e.target
     setText(value)
   }
 
-  console.log(text)
-
+  function wordCount(text) {
+    const wordsArr = text.trim().split(" ")
+    return wordsArr.filter(word => word !== "").length
+  }
+  
   return (
     <div>
       <h1>Typing Game</h1>
@@ -17,8 +29,8 @@ function App() {
               onChange={handleChange}
               value={text}
             />
-            <h4>Time Remaining: </h4>
-            <button>Start</button>
+            <h4>Time Remaining: {timer}</h4>
+            <button onClick={() => wordCount(text)}>Start</button>
             <h1>Word Count: </h1>
     </div>
   );
