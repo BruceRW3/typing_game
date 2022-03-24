3a377338653901f9af38bc5a5312efc8018e99aa
BruceRW3 <brwiggleston@gmail.com>
2022-03-24 09:28:35 -0400

    added handleChange function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function App() {
+  const [text, setText] = useState('')
+
+  function handleChange(e) {
+    const {value} = e.target
+    setText(value)
+  }
+
+  console.log(text)
+
   return (
     <div>
       <h1>Typing Game</h1>
-            <textarea />
+            <textarea 
+              onChange={handleChange}
+              value={text}
+            />
             <h4>Time Remaining: </h4>
             <button>Start</button>
             <h1>Word Count: </h1>
